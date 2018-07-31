b9fd4163c7792414ced2b61de7809382f9cac419
sjbeskur <sam.beskur@gmail.com>
2018-07-31 16:39:07 -0600

    add ingress and docker changes

--- src/publish.sh
+++ src/publish.sh
@@ -3,7 +3,7 @@
 pushd www
 npm install
 npm run build
-dotnet publish --output ../target -r linux-x64
+dotnet publish --output ../target -r linux-x64 # linux-musl-x64 # use this one for alpine linux
 popd
 
 docker build -t www-test . -f Dockerfile.msft-dotnet
