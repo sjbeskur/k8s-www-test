c9a866c4029afddff0128fd9091c788be9b75c12
sjbeskur <sam.beskur@gmail.com>
2018-08-04 13:15:22 -0600

    fix urls

--- push.sh
+++ push.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 aws ecr get-login --no-include-email | bash
+
 aws ecr describe-repositories
 
-docker tag www-test:v2 855281110945.dkr.ecr.us-east-2.amazonaws.com/nmsrepo:www-test
+docker tag www-test:v2 <aws-repo-url>/<repo-name>:www-test
 
-docker push 855281110945.dkr.ecr.us-east-2.amazonaws.com/nmsrepo:www-test
+docker push <aws-repo-url>/<repo-name>:www-test
 
