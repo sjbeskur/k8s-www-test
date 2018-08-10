9f96815fdb8c1cbda28018171f821916c34c190f
sjbeskur <sam.beskur@gmail.com>
2018-08-10 15:02:46 -0600

    test backend service communications (v2.1)

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir target/
 
-docker build -t www-test-build .
+docker build -t www-test-build . -f build/Dockerfile
 docker run -it --rm  -v $PWD/target:/target www-test-build
 
 docker build -t www-test . -f build/Dockerfile.msft-dotnet
