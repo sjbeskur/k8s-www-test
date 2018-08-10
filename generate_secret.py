5865709162be40bb74d3c97e8ebb9e7aea1b085c
sjbeskur <sam.beskur@gmail.com>
2018-08-10 14:18:02 -0600

    add gen secret python script

--- generate_secret.py
+++ generate_secret.py
@@ -16,8 +16,8 @@ def execute_cmd(cmd):
 def generate_secret_key():
   login_cmd = execute_cmd('aws ecr get-login --no-include-email').rstrip('\n')
   creds = re.sub(r"(-e none\ |docker login\ |-u\ |-p\ )", '', login_cmd).split(' ')
-  generate_secret_cmd = "kubectl create secret docker-registry {0} --docker-username={1} --docker-password={2} --docker-server={3} --docker-email=YOUR_EMAIL_ADDRESS"
-  execute_cmd(generate_secret_cmd.format('YOUR_SECRET_KEY_NAME', creds[0], creds[1], creds[2].replace('https://', '')))
+  generate_secret_cmd = "kubectl create secret docker-registry {0} --docker-username={1} --docker-password={2} --docker-server={3} --docker-email=sam.beskur@gmail.com"
+  execute_cmd(generate_secret_cmd.format('samreg', creds[0], creds[1], creds[2].replace('https://', '')))
 
 if __name__ == "__main__":
   generate_secret_key()
